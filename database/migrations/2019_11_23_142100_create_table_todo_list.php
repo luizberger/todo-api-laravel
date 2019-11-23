e5e0779663e7f4844c8d7b0bb11eb95783230e47
Luiz Berger <luizcarlosvieirab@gmail.com>
2019-11-23 12:04:47 -0300

    todo_list table migration

---
+++ database/migrations/2019_11_23_142100_create_table_todo_list.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableTodoList extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('todo_list', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('title');
+            $table->string('desc');
+            $table->boolean('done');
+            $table->timestamps(); // created_at && updated_at
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('todo_list');
+    }
+}
