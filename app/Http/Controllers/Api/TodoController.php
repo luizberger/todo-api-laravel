963b38a0436a5c6e6a7cf9f974b934c5039021b3
Luiz Berger <luizcarlosvieirab@gmail.com>
2019-11-24 20:57:20 -0300

    Add error handler to all endpoints

--- app/Http/Controllers/Api/TodoController.php
+++ app/Http/Controllers/Api/TodoController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Todo;
+use App\API\ApiError;
 
 class TodoController extends Controller
 {
@@ -14,32 +15,63 @@ class TodoController extends Controller
     	return response()->json($data);
     }
 
-    public function show(Todo $id){
-    	$data = ['data' => $id];
+    public function show($id){
+        $todo = Todo::find($id);
+
+        if (!$todo) return response()->json(ApiError::errorMessage('To-do not found', 1), 404);
+
+    	$data = ['data' => $todo];
     	return response()->json($data);
     }
 
     public function add(Request $request){
-        $todoData = $request->all();
-        Todo::create($todoData);
 
-        $responseData = ['msg' => 'To-do created!'];
-        return response()->json($responseData, 201);
+        try {
+            $todoData = $request->all();
+            Todo::create($todoData);
+
+            $responseData = ['msg' => 'To-do created!'];
+            return response()->json($responseData, 201);
+        } catch (\Exception $e) {
+            if (config('app.debug')) {
+                return response()->json(ApiError::errorMessage($e->getMessage(), 1), 500);
+            }
+
+            return response()->json(ApiError::errorMessage('Error inserting to-do', 1), 500);
+        }
+
     }
 
     public function delete(Todo $id){
-        $id->delete();
-        $responseData = ['msg' => 'Todo deleted!', 'data' => $id];
-        return response()->json($responseData);
+        try {
+            $id->delete();
+            $responseData = ['msg' => 'Todo deleted!', 'data' => $id];
+            return response()->json($responseData);
+        } catch (\Exception $e) {
+            if (config('app.debug')) {
+                return response()->json(ApiError::errorMessage($e->getMessage(), 1), 500);
+            }
+
+            return response()->json(ApiError::errorMessage('Error deleting to-do', 1), 500);
+        }
+
     }
 
     public function update(Request $request, $id){
-        $todoData = $request->all();
-        $todo = Todo::find($id);
-        $todo->update($todoData);
+        try {
+            $todoData = $request->all();
+            $todo = Todo::find($id);
+            $todo->update($todoData);
 
-        $responseData = ['msg' => 'To-do updated!'];
-        return response()->json($responseData, 201);
+            $responseData = ['msg' => 'To-do updated!'];
+            return response()->json($responseData, 201);
+        } catch (\Exception $e) {
+            if(config('app.debug')){
+                return response()->json(ApiError::errorMessage($e->getMessage(), 1), 500);
+            }
+
+            return response()->json(ApiError::errorMessage('Error updating to-do', 1), 500);
+        }
     }
 
 }
